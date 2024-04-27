5539cc8f1872fcbc71830cea2468e97c2e7ae179
Batuhan Şahin <115935470+batuhansahin1@users.noreply.github.com>
2024-04-27 19:16:17 +0300

    Update searchView.js

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -15,26 +15,9 @@ class SearchView {
     this._clearInput();
     return input;
   }
-  // renderError(message = this._errMessage) {
-  //   const markup = `<div class="error">
-  //   <div>
-  //     <svg>
-  //       <use href="${icons}#icon-alert-triangle"></use>
-  //     </svg>
-  //   </div>
-  //   <p> ${message}</p>
-  // </div> `;
-  //   this._clear();
-  //   this._parentEl.insertAdjacentHTML('afterbegin', markup);
-  // }
+
   addHandlerSearch(handlerfunc) {
-    //burada formu seçip submit eventi desem de butona click eventi desem de ikisi de aynı işlemi gerçekleştiriyor ikisnde de searchQuery'i girip enter yaptığında ya da mouse ile butona tıkladığında veri gönderiliyor
-    // document
-    //   .querySelector('.search__btn')
-    //   .addEventListener('click', function (e) {
-    //     e.preventDefault();
-    //     handlerfunc();
-    //   });
+    
     this._parentEl.addEventListener('submit', function (event) {
       event.preventDefault();
       handlerfunc();
@@ -43,9 +26,4 @@ class SearchView {
 }
 
 export default new SearchView();
-//sadece tarif eklenen tariflerde kullanılacak
-// <div class="preview__user-generated">
-//   <svg>
-//     <use href="${icons}#icon-user"></use>
-//   </svg>
-//</div>
+
